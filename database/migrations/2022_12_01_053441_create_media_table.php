a9be17c1db815666ff6217f6931bd68bf125c298
ikeogu <ikeogu322@gmail.com>
2022-12-27 19:50:46 +0100

    made model_is to be uuid

--- database/migrations/2022_12_01_053441_create_media_table.php
+++ database/migrations/2022_12_01_053441_create_media_table.php
@@ -10,7 +10,9 @@ return new class extends Migration
     {
         Schema::create('media', function (Blueprint $table) {
             $table->uuid('id')->primary()->unique();
+
             $table->morphs('model');
+            $table->uuid('uuid')->nullable()->unique();
             $table->string('collection_name');
             $table->string('name');
             $table->string('file_name');

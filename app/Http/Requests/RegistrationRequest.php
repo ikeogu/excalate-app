e4569b79b86640d6d584652bba6a8fa46e87ef39
ikeogu <ikeogu322@gmail.com>
2022-12-31 02:04:46 +0100

    added required messages

--- app/Http/Requests/RegistrationRequest.php
+++ app/Http/Requests/RegistrationRequest.php
@@ -43,4 +43,32 @@ class RegistrationRequest extends FormRequest
             'data.relationships.business_profile.data.category_id' => ['nullable', 'string', 'exists:business_categories,id'],
         ];
     }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+
+            'data.attributes.email.unique' => 'Email already exists',
+            'data.attributes.phone_number.unique' => 'Phone number already exists',
+            'data.attributes.password' => 'Password must be at least 8 characters long and must contain at least one uppercase letter, one lowercase letter, one number and one special character',
+            'data.attributes.first_name.required' => 'First name is required',
+            'data.attributes.last_name.required' => 'Last name is required',
+            'data.attributes.phone_number.required' => 'Phone number is required',
+            'data.attributes.password.required' => 'Password is required',
+            'data.attributes.email.required' => 'Email is required',
+            'data.attributes.first_name.max' => 'First name must not be more than 255 characters',
+            'data.attributes.last_name.max' => 'Last name must not be more than 255 characters',
+            'data.attributes.phone_number.max' => 'Phone number must not be more than 255 characters',
+            'data.attributes.password.min' => 'Password must be at least 8 characters long',
+            'data.attributes.email.email' => 'A valid email is required',
+            'data.attributes.avatar.mimes' => 'Avatar must be a file of type: png, jpg, jpeg',
+            'data.attributes.avatar.file' => 'Avatar must be a file of type: png, jpg, jpeg',
+            
+        ];
+    }
 }
